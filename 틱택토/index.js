ae5a4cc614c3699729c94bdf43d4214ba6d90019
yjglab <yjgdesign@gmail.com>
2022-02-07 03:40:15 +0900

    검사 함수 update

--- 틱택토/index.js
+++ 틱택토/index.js
@@ -11,7 +11,44 @@ for (let i = 0; i < 3; i++) {
   }
 }
 
-let comTurn;
-let myTurn;
+let com = "X";
+let my = "O";
+let turn = 1; // 1: my, 2: com
+const $container = document.querySelector(".container");
+const inspectItems = (who) => {
+  let cnt = 0;
+  // 열, 행 검사
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (spaceArray[i][j].textContent === who) cnt++;
+    }
+    if (cnt === 3) console.log("일치!");
+    // 리턴해야함
+    else cnt = 0;
+  }
+  // 대각 검사
+  cnt = 0;
+  for (let i = 0; i < 3; i++) {
+    if (spaceArray[i][i].textContent === who) cnt++;
+  }
+  if (cnt === 3) console.log("대각 일치"); // 리턴해야함
+  if (
+    spaceArray[0][2].textContent === who &&
+    spaceArray[1][1].textContent === who &&
+    spaceArray[2][0].textContent === who
+  ) {
+    console.log("대각 일치"); // 리턴해야함
+  }
+};
+const handleItemClick = (e) => {
+  if (e.target.textContent !== "" || turn !== 1) return;
+  e.target.textContent = my;
 
+  // 검사
+  inspectItems(my); // true or false
+};
+$items.forEach((item) => item.addEventListener("click", handleItemClick));
+// $item.addEventListener("click", handleItemClick);
 // spaceArray[0][1].textContent = "o";
+// com은 $items[] 인덱스에서 랜덤 숫자로 뽑으면 될듯?
+// 검사 함수는 따로 빼면 좋을듯
