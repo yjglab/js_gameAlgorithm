4c0c88ca77fe9c06aff42a44af77d52a2c361707
yjglab <yjgdesign@gmail.com>
2022-02-14 19:10:16 +0900

    이미지 업 다운, 클릭이벤트 구현

--- 고양이잡기/index.js
+++ 고양이잡기/index.js
@@ -2,8 +2,8 @@ const $button = document.querySelector("button");
 const $time = document.querySelector(".time");
 const $table = document.querySelector("table");
 const $score = document.querySelector(".score");
+let time = 1;
 const init = () => {
-  let time = 1;
   setInterval(() => {
     $time.textContent = time++;
   }, 1000);
@@ -21,9 +21,47 @@ const init = () => {
   // cat or bomb 보이기
   display();
 };
+
 function display() {
   // 캣 4, 봄 1 비율 어레이
-  const itemsArray = ["catup.jpg", "catup.jpg", "catup.jpg", "bomb.jpg"];
+  const $td = document.querySelectorAll("td");
+  console.log($td);
+  const itemsArray = [
+    "catup.jpg",
+    "catup.jpg",
+    "catup.jpg",
+    "catup.jpg",
+    "catup.jpg",
+    "bombup.jpg",
+  ];
+  setInterval(() => {
+    let randomItemImg =
+      itemsArray[Math.floor(Math.random() * itemsArray.length)];
+    const randomIdx = Math.floor(Math.random() * $td.length);
+    if (!$td[randomIdx].innerHTML) {
+      if (randomItemImg === "catup.jpg") {
+        $td[randomIdx].innerHTML = `<img src=${randomItemImg} title="cat" />`;
+        setTimeout(() => {
+          $td[randomIdx].querySelector("img").classList.add("up");
+        }, 0);
+      } else if (randomItemImg === "bombup.jpg") {
+        $td[randomIdx].innerHTML = `<img src=${randomItemImg} title="bomb" />`;
+        setTimeout(() => {
+          $td[randomIdx].querySelector("img").classList.add("up");
+        }, 0);
+      }
+    }
+  }, 1000);
+  $table.addEventListener("click", handleClickItems);
+}
+function handleClickItems(e) {
+  console.log(e.target);
+  if (e.target.title === "cat") {
+    e.target.classList = "down";
+    setTimeout(() => {
+      e.target.remove();
+    }, 500);
+  }
 }
 // $button.addEventListener("click", init);
 init();
